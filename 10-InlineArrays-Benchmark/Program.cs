77be9590be7734e9c1b3f5074352502cc5a5074f
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2023-07-31 15:49:40 +0200

    Add InlineArrays

---
+++ 10-InlineArrays-Benchmark/Program.cs
@@ -0,0 +1,94 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Runtime.CompilerServices;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private readonly Random random = new Random(256);
+
+    [Benchmark]
+    public int[] Create_Array()
+    {
+        var array = new int[1000];
+
+        for (int i = 0; i < 1000; i++)
+        {
+            array[i] = random.Next();
+        }
+
+        return array;
+    }
+
+    [Benchmark]
+    public MyArray<int> Create_MyArray()
+    {
+        var array = new MyArray<int>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            array[i] = random.Next();
+        }
+
+        return array;
+    }
+
+
+    private int[] _preArray = new int[1000];
+    private MyArray<int> _preInlineArray = new MyArray<int>();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Random random = new Random(256);
+
+        for (int i = 0; i < 1000; i++)
+        {
+            _preArray[i] = random.Next();
+        }
+
+        random = new Random(256);
+        for (int i = 0; i < 1000; i++)
+        {
+            _preInlineArray[i] = random.Next();
+        }
+    }
+
+
+    [Benchmark]
+    public int Get_Array()
+    {
+        var sum = 0;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            sum += _preArray[i];
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int Get_MyArray()
+    {
+        var sum = 0;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            sum += _preInlineArray[i];
+        }
+
+        return sum;
+    }
+
+}
+
+
+
+[InlineArray(1000)]
+public struct MyArray<T>
+{
+    private readonly T _element;
+}
---
+++ 10-InlineArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+var array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = i;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+var myarray = new MyArray<int>();
+
+
+for (int i = 0; i < 10; i++)
+{
+    array[i] = i;
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+
+[InlineArray(10)]
+public struct MyArray<T>
+{
+    private readonly T _element;
+}
