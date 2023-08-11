9625ade6c7f7a2fae9b3a32124c0ef8a9a4a0070
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2023-08-11 15:54:32 +0200

    add 18-Interceptors

---
+++ 18-Interceptors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+Example ex = new Example();
+ex.MyMethod("Check");
+ex.MyMethod("Check");
+ex.MyMethod("Check");
+ex.MyMethod("Check");
+
+public static class Code
+{
+    [InterceptsLocation("C:\\Users\\Admin\\source\\repos\\WhatIsNewInCsharp12AndNET8\\18-Interceptors\\Program.cs",
+        line: 5, column: 4)]
+    public static void MyInterceptorMethod(this Example ex, string param)
+    {
+        Console.WriteLine($"MyInterceptorMethod {param}");
+    }
+}
+
+public class Example
+{
+    public void MyMethod(string param)
+    {
+        Console.WriteLine($"MyMethod {param}");
+    }
+}
+
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    sealed class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute
+    {
+    }
+}
+
+//https://andrewlock.net/exploring-the-dotnet-8-preview-changing-method-calls-with-interceptors/
